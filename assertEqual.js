f5e2ab28e82c20c44f92e03db0c567f46ebf2d51
emtupp <emilyc.tupper@gmail.com>
2020-10-26 20:34:43 +0000

    Creating the assertEqual function to verify code

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('👌Assertion Passed: ' + actual + ' === ' + expected + '👌');
+  } else {
+    console.log('❗Assertion Failed: ' + actual + ' !== ' + expected + '❗');
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(10, 8);
