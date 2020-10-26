7fea5b7ddbdba20f7b244eedf25b5f890b8b878d
emtupp <emilyc.tupper@gmail.com>
2020-10-26 21:36:23 +0000

    adding tail.js and updated head.js to the repo

--- head.js
+++ head.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
-  } else {
-    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
-  }
-};
-
 const head = function(array){
   return array[0];
 }
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
