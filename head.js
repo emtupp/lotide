2952dcef09678e2a3c0b9e1ae86001238c88fd42
emtupp <emilyc.tupper@gmail.com>
2020-10-26 21:04:01 +0000

    Adding head.js and updated assertEqual.js to repo

--- head.js
+++ head.js
@@ -5,3 +5,10 @@ const assertEqual = function(actual, expected) {
     console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
   }
 };
+
+const head = function(array){
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
