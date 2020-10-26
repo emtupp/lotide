dd7bd7b6058864a9e80dad06d011bedf7d498d3a
emtupp <emilyc.tupper@gmail.com>
2020-10-26 21:02:23 +0000

    Fixing assertEqual to read right message

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('👌Assertion Passed: ${actual} === ${expected}👌');
+    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
   } else {
-    console.log('❗Assertion Failed: ${Sactual} !== &{expected}❗');
+    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
   }
 };
+
+assertEqual(1, 2);
+assertEqual(1, 1);
