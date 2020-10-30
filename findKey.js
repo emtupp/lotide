97ffeb6392ac376b5c8a9ddd03f5102284236c8e
emtupp <emilyc.tupper@gmail.com>
2020-10-30 00:51:42 +0000

    adding takeUntil to repo and adding incomplete findKey to repo

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
+  } else {
+    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
+  }
+};
+
+const findKey = function(objects, callback){
+  for (object in Object.keys(object)){}
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
