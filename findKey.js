3bbfcb4c76070d4eb35c5364f3be18bc21f4eb33
emtupp <emilyc.tupper@gmail.com>
2020-10-30 01:39:00 +0000

    adding finished findKey to repo

--- findKey.js
+++ findKey.js
@@ -6,15 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKey = function(objects, callback){
-  for (object in Object.keys(object)){}
-}
-
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+const findKey = function(objects, callback) {
+  for (let item in objects) {
+    if (callback(objects[item])) {
+      return console.log(item);
+    }
+  }
+};
