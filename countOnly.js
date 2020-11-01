d4f74c954559b0eb3c3d6d88cf534aaa7830a7b4
emtupp <emilyc.tupper@gmail.com>
2020-11-01 23:56:14 +0000

    modifying assertEqual to use into countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
-  } else {
-    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const countOnly = function(allItems, itemsToCount) {
   let answer = {};
@@ -19,22 +13,3 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return answer;
 };
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
