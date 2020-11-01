96943146ae43c1e277c717e7f2972ddfdeb07a83
emtupp <emilyc.tupper@gmail.com>
2020-11-01 23:33:47 +0000

    fixed eqArrays and modified to export to assertArraysEqual'

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let trueCount = 0;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2 [i]) {
-      trueCount += 1;
-    }
-  }
-  if (trueCount === array2.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2){
   console.log(eqArrays(array1, array2));
--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,19 @@
-const eqArrays = function(array1, array2) {
-  let trueCount = 0;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2 [i]) {
-      trueCount += 1;
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let trueCount = 0;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        trueCount += 1;
+      }
+    }
+    if (trueCount === array2.length) {
+      return true;
+    } else {
+      return false;
     }
   }
-  if (trueCount === array2.length) {
-    return true;
-  } else {
-    return false;
-  }
 };
+
+module.exports = eqArrays;
