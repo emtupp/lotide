64ab2be592843f643b49238af191a73cf51820eb
emtupp <emilyc.tupper@gmail.com>
2020-11-02 00:01:42 +0000

    cleaned up code for countLetters and findKeyByValue

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
-  } else {
-    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const countLetters = function(sentence) {
   let letterCounter = {};
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌Assertion Passed: ${actual} === ${expected}👌`);
-  } else {
-    console.log(`❗Assertion Failed: ${actual} !== ${expected}❗`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const findKeyByValue = function(objectList, keyValue) {
   for (let keyName in objectList) {
@@ -13,13 +7,3 @@ const findKeyByValue = function(objectList, keyValue) {
     }
   }
 };
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
