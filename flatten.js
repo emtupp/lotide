bd3aa6be2c928effc00fbc15f4239cc334e7de68
emtupp <emilyc.tupper@gmail.com>
2020-11-02 23:33:29 +0000

    adding README file and finished flatten()

--- flatten.js
+++ flatten.js
@@ -1,1 +1,18 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
+const flattenedArr = [];
+
+const flatten = function(arr) {
+  arr.forEach((element) => {
+    if (Array.isArray(element)) {
+      flatten(element);
+    } else {
+      flattenedArr.push(element);
+    }
+  });
+  return flattenedArr;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
 module.exports = flatten;
