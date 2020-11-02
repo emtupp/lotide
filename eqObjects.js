f5dbe4abc2e94ed570e5b6173766620f1b530c2e
emtupp <emilyc.tupper@gmail.com>
2020-11-02 01:17:08 +0000

    adding finished eqObjects to repo

--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,14 @@
 const assertEqual = require('./assertEqual');
 
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    let trueCount = 0;
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        trueCount += 1;
-      }
-    }
-    if (trueCount === array2.length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
+  } else if (!eqArrays(Object.keys(object1), Object.keys(object2))) {
+    return false;
   } else {
     for (let key of Object.keys(object1)) {
-      // console.log('key is ' + key)
-      // console.log('object1[key] is ' + object1[key])
-      // console.log('object2[key] is ' + object2[key])
       if (Array.isArray(object1[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
@@ -40,20 +23,8 @@ const eqObjects = function(object1, object2) {
     }
     return true;
   }
-
-  
-  // else {
-  //   for (let keyName in Object.keys(object1)) {
-  //     if (Array.isArray(object1[keyName]) && Array.isArray(object2[keyName])) {
-  //       return eqArrays(object1[keyName], object2[keyName]);
-  //     } else if (object1[keyName] === object2[keyName]) {
-  //       continue;
-  //     } else {
-  //       return false;
-  //     }
-  //   }
-  // }
-  // return true;
 };
 
-console.log(eqObjects({a: {a:1, b:2}, b: [3, 'dog', [1, 2, 3]], c: 4}, {a: {a:1, b:2}, b: [3, 'dog', [1, 2, 3]], c: 4}))
+console.log(eqObjects({a: {a:1, b:2}, b: [3, 'dog'], c: 4}, {a: {a:1, b:2}, b: [3, 'dog'], c: 4}))
+
+// could be improved by adding recursion if input is nested array
