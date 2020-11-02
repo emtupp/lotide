6b193220e5edd1b75c1f733eed24d897d945bf12
emtupp <emilyc.tupper@gmail.com>
2020-11-02 18:24:25 +0000

    added empty flatten.js, and added test/assertEqualTest.js. Readded test code to files.

--- head.js
+++ head.js
@@ -1,3 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 };
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
