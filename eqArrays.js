27cd4400a29698eb4afd484494fef4c4aea8cd28
emtupp <emilyc.tupper@gmail.com>
2020-11-03 00:01:27 +0000

    implementing recursice eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -2,17 +2,16 @@ const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    let trueCount = 0;
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        trueCount += 1;
+      if (Array.isArray(array1[i])) {
+        eqArrays(array1[i], array2[i]);
+      } else if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        continue;
       }
     }
-    if (trueCount === array2.length) {
-      return true;
-    } else {
-      return false;
-    }
+    return true;
   }
 };
 
--- test1.js
+++
@@ -1,3 +0,0 @@
-const flatten = require('./flatten');
-
-console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
